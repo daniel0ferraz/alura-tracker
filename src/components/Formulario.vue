05155950984117db7629c49bb04463ddc0006055
Daniel Ferraz <Daniel0ferraz@outlook.com>
2022-08-16 16:37:55 -0300

    refatorando componentes

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -34,6 +34,10 @@
             <span>stop</span>
           </button>
         </div>
+
+        <div class="column">
+          <Temporizador />
+        </div>
       </div>
     </div>
   </div>
@@ -41,31 +45,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Temporizador from './Temporizador.vue';
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: 'Formulario',
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0,
-    };
-  },
-  computed: {
-    tempoDecorrido(): string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
-    },
-  },
-  methods: {
-    iniciar() {
-      // começar a contagem
-      // 1 seg = 1000 ms
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1;
-      }, 1000);
-    },
-    finalizar() {
-      clearInterval(this.cronometro);
-    },
+  component: {
+    Temporizador,
   },
 });
 </script>
---
+++ src/components/Temporizador.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Formulario',
+  data() {
+    return {
+      tempoEmSegundos: 0,
+      cronometro: 0,
+    };
+  },
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8);
+    },
+  },
+  methods: {
+    iniciar() {
+      // começar a contagem
+      // 1 seg = 1000 ms
+      this.cronometro = setInterval(() => {
+        this.tempoEmSegundos += 1;
+      }, 1000);
+    },
+    finalizar() {
+      clearInterval(this.cronometro);
+    },
+  },
+});
+</script>
